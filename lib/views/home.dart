680cdcf795686998278162e3b89cd733e68198c5
AYJF <jimenezfernandezalvaro@gmail.com>
2021-11-26 13:59:12 -0600

    set background

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -5,9 +5,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-      child: Text("Hello"),
-    ));
+    return Scaffold(
+        extendBodyBehindAppBar: true,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: const Center(
+            child: Text("Hello"),
+          ),
+        ));
   }
 }
